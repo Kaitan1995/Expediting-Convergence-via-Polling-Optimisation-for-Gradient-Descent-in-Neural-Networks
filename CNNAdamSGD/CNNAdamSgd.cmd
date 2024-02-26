5ade499b74164b6b9146417de21a4d63381d0f72
Kaitan1995 <renkaitan11@gmail.com>
2024-02-26 19:15:41 +0800

    Updated Names.

--- CNNAdamSGD/CNNAdamSgd.cmd
+++ CNNAdamSGD/CNNAdamSgd.cmd
@@ -1,7 +1,7 @@
 
 @echo off
-rem This script was created by Nuitka to execute 'CNNAdamSgd.exe' with Python DLL being found.
+rem This script was created by Nuitka to execute 'CNNAdamSGD.exe' with Python DLL being found.
 set PATH=C:\Users\Kai\AppData\Local\Programs\Python\PYTHON~1;%PATH%
 set PYTHONHOME=C:\Users\Kai\AppData\Local\Programs\Python\Python310
-set NUITKA_PYTHONPATH=C:\Users\Kai\Desktop\Expediting-Convergence-via-Polling-Optimisation-for-Gradient-Descent-in-Neural-Networks\CNNAdamSGD;C:\Users\Kai\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Kai\AppData\Local\Programs\Python\Python310\lib;C:\Users\Kai\AppData\Local\Programs\Python\Python310;C:\Users\Kai\Desktop\Expediting-Convergence-via-Polling-Optimisation-for-Gradient-Descent-in-Neural-Networks\CNNAdamSGD\CNNAdamSGDEnv;C:\Users\Kai\Desktop\Expediting-Convergence-via-Polling-Optimisation-for-Gradient-Descent-in-Neural-Networks\CNNAdamSGD\CNNAdamSGDEnv\lib\site-packages
-"%~dp0CNNAdamSgd.exe" %*
+set NUITKA_PYTHONPATH=C:\Users\Kai\Desktop\(Private) Polling Method\CNNAdamSGD;C:\Users\Kai\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Kai\AppData\Local\Programs\Python\Python310\lib;C:\Users\Kai\AppData\Local\Programs\Python\Python310;C:\Users\Kai\Desktop\(Private) Polling Method\CNNAdamSGD\CNNAdamSGDEnv;C:\Users\Kai\Desktop\(Private) Polling Method\CNNAdamSGD\CNNAdamSGDEnv\lib\site-packages
+"%~dp0CNNAdamSGD.exe" %*
--- CNNAdamSGDPolling/CNNAdamSGDPolling.cmd
+++ CNNAdamSGDPolling/CNNAdamSGDPolling.cmd
@@ -3,5 +3,5 @@
 rem This script was created by Nuitka to execute 'CNNAdamSGDPolling.exe' with Python DLL being found.
 set PATH=C:\Users\Kai\AppData\Local\Programs\Python\PYTHON~1;%PATH%
 set PYTHONHOME=C:\Users\Kai\AppData\Local\Programs\Python\Python310
-set NUITKA_PYTHONPATH=C:\Users\Kai\Desktop\Expediting-Convergence-via-Polling-Optimisation-for-Gradient-Descent-in-Neural-Networks\CNNAdamSGDPolling;C:\Users\Kai\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Kai\AppData\Local\Programs\Python\Python310\lib;C:\Users\Kai\AppData\Local\Programs\Python\Python310;C:\Users\Kai\Desktop\Expediting-Convergence-via-Polling-Optimisation-for-Gradient-Descent-in-Neural-Networks\CNNAdamSGDPolling\CNNAdamSGDPollingEnv;C:\Users\Kai\Desktop\Expediting-Convergence-via-Polling-Optimisation-for-Gradient-Descent-in-Neural-Networks\CNNAdamSGDPolling\CNNAdamSGDPollingEnv\lib\site-packages
+set NUITKA_PYTHONPATH=C:\Users\Kai\Desktop\(Private) Polling Method\CNNAdamSGDPolling;C:\Users\Kai\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Kai\AppData\Local\Programs\Python\Python310\lib;C:\Users\Kai\AppData\Local\Programs\Python\Python310;C:\Users\Kai\Desktop\(Private) Polling Method\CNNAdamSGDPolling\CNNAdamSGDPollingEnv;C:\Users\Kai\Desktop\(Private) Polling Method\CNNAdamSGDPolling\CNNAdamSGDPollingEnv\lib\site-packages
 "%~dp0CNNAdamSGDPolling.exe" %*
